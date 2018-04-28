9257db14adc64e285b97318bef61d30f698d8efe
DaniFoldi <dani@stefan.hu>
2018-04-28 16:00:29 +0200

    Changed to constants for easier setting

--- bidirectional_discovery.py
+++ bidirectional_discovery.py
@@ -2,6 +2,9 @@ import socket
 import _thread
 import time
 
+DEFAULT_PORT = 65000
+DEFAULT_DELAY = 3
+
 class Discovery:
     def __init__(self, data, address, time):
         self.data = data
@@ -16,27 +19,25 @@ class Bidirectional_discovery:
         self.announcement_service.bind(('', 0))
         self.announcement_service.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        self.default_port = 60000
-        self.default_delay = 3
         self.stopped = True
 
-    def start_announcement(self, data, delay=self.default_delay, port=self.default_port):
+    def start_announcement(self, data, delay=DEFAULT_DELAY, port=DEFAULT_PORT):
         self.stopped = False
         _thread.start_new(self.announce, (data, delay, port))
 
-    def start_discovery(self, callback, delay=self.default_delay, port=self.default_port):
+    def start_discovery(self, callback, delay=DEFAULT_DELAY, port=DEFAULT_PORT):
         self.stopped = False
         self.discovery_service.bind(('', port))
         self.discovered = []
         _thread.start_new(self.discover, (callback, port))
         _thread.start_new(self.handle_discoveries, (delay))
 
-    def announce(self, data, delay=self.default_delay, port=self.default_port):
+    def announce(self, data, delay=DEFAULT_DELAY, port=DEFAULT_PORT):
         while not self.stopped:
             self.announcement_service.sendto(data.encode(), ('<broadcast>', port))
             time.sleep(delay)
 
-    def discover(self, callback, port=self.default_port):
+    def discover(self, callback, port=DEFAULT_PORT):
         while not self.stopped:
             data, address = self.discovery_service.recvfrom(1024)
             data = data.decode()
@@ -44,7 +45,7 @@ class Bidirectional_discovery:
                 self.discovered.append(Discovery(data, address, int(time.strftime("%S", time.gmtime()))))
                 callback(data)
 
-    def handle_discoveries(self, delay=self.default_delay):
+    def handle_discoveries(self, delay=DEFAULT_DELAY):
         while not self.stopped:
             for discovery in range(len(self.discovered)):
                 if self.discovered[discovery].time > int(time.strftime("%S", time.gmtime())) + delay:
