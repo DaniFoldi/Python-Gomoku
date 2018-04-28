0691ad8924e41f62ff4820832ab120511da8555c
DaniFoldi <dani@stefan.hu>
2018-04-28 21:05:20 +0200

    General bugfix, updates
    Using the new classes

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import tkinter.ttk as ttk
 import _thread
 import re
 
+from bidirectional_discovery import Bidirectional_discovery
+from bidirectional_communication import Bidirectional_communication
+from rsa_encryption import RSA_encryption
+
 class ConnectButton():
     def __init__(self, frame, onclick, value):
         self.connect_button = tk.Button(frame, text="Connect", command=self.clicked)
@@ -23,7 +27,7 @@ class GridButton():
         self.y = y
 
     def clicked(self):
-        self.onclick(x, y)
+        self.onclick(self.x, self.y)
 
 class GameWindow():
     def center_window(self):
@@ -50,16 +54,19 @@ class GameWindow():
         else:
             self.window.title("Gomoku - {}".format(state))
 
-    def display_gui(self):
+    def clear_frame(self):
         if hasattr(self, "menu_frame"):
             self.menu_frame.destroy()
         self.menu_frame = tk.Frame(self.window)
         self.menu_frame.place(relx=.5, rely=.5, anchor="c")
 
+    def display_gui(self, state):
+        self.clear_frame()
+
         button_font = font.Font(size="20")
         label_font = font.Font(size="24")
 
-        if self.application_state == 0:
+        if state == 0:
             local_multiplayer_button = tk.Button(self.menu_frame, text="Local Multiplayer", font=button_font, command=self.setup_local_game)
             local_multiplayer_button.grid(row=0, columnspan=2, sticky="NSEW")
             connect_button = tk.Button(self.menu_frame, text="Connect", font=button_font, command=self.discover_hosts)
@@ -67,7 +74,7 @@ class GameWindow():
             create_button = tk.Button(self.menu_frame, text="Create Game", font=button_font, command=self.setup_game)
             create_button.grid(row=1, column=1, sticky=tk.NSEW)
 
-        if self.application_state == 1:
+        if state == 1:
             width_label = tk.Label(self.menu_frame, text="Grid width:", font=label_font)
             width_label.grid(row=0, column=0, sticky="w")
             self.width_value = tk.IntVar(self.menu_frame, value=20)
@@ -98,7 +105,7 @@ class GameWindow():
             go_button = tk.Button(self.menu_frame, text="Confirm", command=self.start_loading)
             go_button.grid(row=7, columnspan=2)
 
-        if self.application_state == 2:
+        if state == 2:
             port_label = tk.Label(self.menu_frame, text="Port:", font=label_font)
             port_label.grid(row=0, column=0, sticky="w")
             self.port_value = tk.IntVar(self.menu_frame, value=65530)
@@ -107,29 +114,26 @@ class GameWindow():
             go_button = tk.Button(self.menu_frame, text="Confirm", command=self.discover_hosts)
             go_button.grid(row=1, columnspan=2)
 
-        if self.application_state == 4:
-            searching_label = tk.Label(self.menu_frame, text="Searching...", font=label_font)
-            searching_label.grid(row=0)
-
-        if self.application_state == 3:
+        if state == 3:
             waiting_label = tk.Label(self.menu_frame, text="Waiting...", font=label_font)
             waiting_label.grid(row=0)
 
-        if self.application_state == 5:
+        if state == 4:
+            searching_label = tk.Label(self.menu_frame, text="Searching...", font=label_font)
+            searching_label.grid(row=0)
+
+        if state == 5:
             self.game_frame = tk.Frame(self.menu_frame)
             self.game_frame.grid(row=0, column=0)
 
     def discover_hosts(self):
-        self.connected = False
-        self.cancelled = False
-        self.application_state = 4
-        self.display_gui()
+        self.display_gui(4)
         self.connection_options = []
-        self.start_discovery()
+        self.connection_frames = []
+        self.discovery.start_discovery(self.new_server_found, self.server_timeout)
 
     def start_network_game(self):
-        self.application_state = 5
-        self.display_gui()
+        self.display_gui(5)
         self.game_grid = []
         for i in range(self.grid_height):
             self.game_grid.append([])
@@ -146,36 +150,39 @@ class GameWindow():
         if self.local_game:
             start_local_game()
         else:
-            self.application_state = 3
-            self.display_gui()
-            self.connection_from_other = socket.socket()
-            self.connection_from_other.bind((self.local_ip, self.port_value.get() + 1))
-            self.start_announcement(self.width_value.get(), self.height_value.get(), self.port_value.get())
+            self.display_gui(3)
+            self.communication.host(self.local_ip, self.port_value.get() + 1, get_connection, self.get_message)
+            self.discovery.start_announcement("GOMOKU-{}-{}-{}-{}-{}-{}".format(self.version, self.local_ip, self.width_value.get(), self.height_value.get(), self.local_name, self.port_value.get()))
             _thread.start_new(self.wait_for_connection(), ())
         print(self.port_value.get())
 
+    def get_connection(address):
+
+
+    def get_message(message):
+        pass
+
     def wait_for_connection(self):
-        self.connection_from_other.listen(1)
+        self.connection_from_other.listen(5)
         client, address = self.connection_from_other.accept()
         data = client.recv(1024).decode()
         actual_data = get_announced_data(data)
         self.connected = True
+        self.communication.connect(actual_data[1], int(actual_data[5]) + 2)
         if not hasattr(self, "connection_to_other"):
             self.connection_to_other = socket.socket()
         self.connection_to_other.connect((actual_data[1], int(actual_data[5]) + 2))
 
     def start_local_game(self):
-        self.application_state = 5
+        self.display_gui(5)
 
     def setup_game(self):
-        self.application_state = 1
         self.local_game = False
-        self.display_gui()
+        self.display_gui(1)
 
     def setup_local_game(self):
-        self.application_state = 1
         self.local_game = True
-        self.display_gui()
+        self.display_gui(1)
 
     def display_connection_details(self, data):
         frame = tk.Frame(self.menu_frame)
@@ -188,62 +195,42 @@ class GameWindow():
         connect_button = ConnectButton(frame, self.connect_to, len(self.connection_options) - 1)
         connect_button.connect_button.grid(column=1, rowspan=3, row=0, sticky="ns")
         frame.grid(row=len(self.connection_options))
+        self.connection_frames.append(frame)
 
-    def connect_to(self, value):
+    def connect_to(self, id):
         self.connected = True
-        if not hasattr(self, "connection_to_other"):
-            self.connection_to_other = socket.socket()
-        self.connection_to_other.connect((self.connection_options[value][1], int(self.connection_options[value][5]) + 1))
-        self.connection_from_other = socket.socket()
-        self.connection_from_other.bind((self.local_ip, int(self.connection_options[value][5]) + 2))
+        self.communication.connect(self.connection_options[id][1], int(self.connection_options[id][5]) + 1)
+        self.communication.host(self.local_ip, int(self.connection_options[id][5]) + 2, self.get_connection, self.get_message)
 
     def get_announced_data(self, data):
         if not data.startswith("GOMOKU"):
-            return NULL
-        actual_data = data.split("-")
-        return actual_data[1:]
+            return None
+        else:
+            return data.split("-")[1:]
 
-    def start_announcement(self, width, height, port=65530):
-        if not hasattr(self, "announcement"):
-            self.announcement = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.announcement.bind(('', 0))
-            self.announcement.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        _thread.start_new(self.send_announcement, (width, height, port))
+    def new_server_found(self, data):
+        if len(self.connection_options) == 0:
+            self.clear_frame()
+        self.connection_options.append(self.get_announced_data(data))
+        self.display_connection_details(self.get_announced_data(data))
 
-    def send_announcement(self, width, height, port=65530):
-        while not self.connected or not self.cancelled:
-            self.announcement.sendto("GOMOKU-{}-{}-{}-{}-{}-{}".format(self.version, self.local_ip, width, height, self.local_name, port).encode("utf-8"), ('<broadcast>', port))
-            time.sleep(2)
-
-    def discover_announcements(self):
-        while not self.connected or not self.cancelled:
-            data, address = self.discovery.recvfrom(1024)
-            actual_data = self.get_announced_data(data.decode("utf-8"))
-            if address not in [(a[6], a[7]) for a in self.connection_options]:
-                if len(self.connection_options) == 0:
-                    self.menu_frame.destroy()
-                    self.menu_frame = tk.Frame(self.window)
-                    self.menu_frame.place(relx=.5, rely=.5, anchor="c")
-                data_to_use = actual_data + [a for a in address]
-                self.connection_options.append(data_to_use)
-                self.display_connection_details(data_to_use)
-
-    def start_discovery(self, port=65530):
-        if not hasattr(self, "discovery"):
-            self.discovery = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.discovery.bind(('', port))
-        _thread.start_new(self.discover_announcements, ())
+    def server_timeout(self, id):
+        del self.connection_options[id]
+        self.connection_frames[id].destroy()
+        del self.connection_frames[id]
 
     def __init__(self):
-        self.version = "V1.0"
+        self.version = "V1.1"
         self.window = tk.Tk()
         self.center_window()
         self.set_game_state()
-        self.application_state = 0
-        self.display_gui()
+        self.display_gui(0)
         self.local_ip = socket.gethostbyname(socket.gethostname())
         self.local_ip_numbers = [int(a) for a in self.local_ip.split(".")]
         self.local_name = socket.gethostname().replace("-", " ")
+
+        self.discovery = Bidirectional_discovery()
+        self.communication = Bidirectional_communication()
 
 if __name__ == "__main__":
     game = GameWindow()
