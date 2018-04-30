0243840c961cbb775069746e69460ede6966cfce
DaniFoldi <dani@stefan.hu>
2018-04-30 22:33:53 +0200

    Fixed crash on not continuing game

--- bidirectional_communication.py
+++ bidirectional_communication.py
@@ -29,7 +29,7 @@ class Bidirectional_communication:
 
     def disconnect(self):
         self.disconnected = True
-        self.incoming_connection.shutdown(SHUT_RDWR)
-        self.outgoing_connection.shutdown(SHUT_RDWR)
+        self.incoming_connection.shutdown(socket.SHUT_RDWR)
+        self.outgoing_connection.shutdown(socket.SHUT_RDWR)
         self.incoming_connection.close()
         self.outgoing_connection.close()
