e305cbd7b167dea985040bd5c4771a7703331a76
DaniFoldi <dani@stefan.hu>
2018-05-01 12:49:52 +0200

    Fixed indentation and simplified agreement

--- agreement.py
+++ agreement.py
@@ -1,19 +1,17 @@
 class Agreement:
-	def __init__(self, callback):
-		self.local = None
-		self.remote = None
-		self.callback = callback
+    def __init__(self, callback):
+        self.local = None
+        self.remote = None
+        self.callback = callback
 
-	def local_answer(self, answer):
-		self.local = answer
-		self.agree()
+    def local_answer(self, answer):
+        self.local = answer
+        self.agree()
 
-	def remote_answer(self, answer):
-		self.remote = answer
-		self.agree()
+    def remote_answer(self, answer):
+        self.remote = answer
+        self.agree()
 
-	def agree(self):
-		if self.local == False:
-			self.callback(False)
-		elif self.local == True and self.remote is not None:
-			self.callback(self.remote)
+    def agree(self):
+        if self.local is not None and self.remote is not None:
+            self.callback(self.local and self.remote)
